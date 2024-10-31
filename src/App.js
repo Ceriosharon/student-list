b2332777d1b0dbe2a16e84b568ffda93430f2bb9
Ceriosharon <116891779+Ceriosharon@users.noreply.github.com>
2024-10-31 11:40:38 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Student List</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
